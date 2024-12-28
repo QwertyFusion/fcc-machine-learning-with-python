6994feac3c61e9adfb5652bf61edddf4fa1cfef1
Rishi Banerjee <rishibanerjee10@gmail.com>
2024-12-28 14:36:41 +0530

    Upload of starter code for rock paper scissor game

---
+++ fcc-rock-paper-scissors/RPS.py
@@ -0,0 +1,1 @@
+# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
